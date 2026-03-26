2fe5e7ffdb5c6c12c94976edae865fb71294f669
Devidas Dakhare <revati.devidas-dakhare@capgemini.com>
2026-03-26 12:45:01 +0530

    Add CICD workflow and infra

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.14.7"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
