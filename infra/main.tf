b5f1cf0438c44db8c2a67f39f9a5365a34ebcabe
revatidakhare-cg <revati.devidas-dakhare@capgemini.com>
2026-04-01 16:00:42 +0530

    added required provider version in  main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = "1.14.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
 }
 
 provider "azurerm" {
